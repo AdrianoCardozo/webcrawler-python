ca870b77feec9d5510204c1f171534fde580ed56
Adriano Souza <100186292+AdrianoCardozo@users.noreply.github.com>
2022-08-16 20:48:11 -0300

    Add files via upload

---
+++ web_crawler.py
@@ -0,0 +1,16 @@
+import requests
+import re
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+
+# Os headers são para informar ao servidor que estamos acessando o site via navegador, caso ao contrário ele não deixará acessar
+}
+
+website = requests.get("https://www.tripadvisor.com.br/Attraction_Review-g303322-d318128-Reviews-Congresso_Nacional-Brasilia_Federal_District.html", headers=headers)
+avaliacao = re.findall(r'<span class="biGQs _P pZUbB SZRPS KxBGd">([\w.]+) avaliações</span>', website.text)
+nota = re.findall(r'<div class="biGQs _P fiohW therk uuBRH">([\w.]+)</div>', website.text)
+
+#agora é só printar usando o numero 0
+
+print(f'Nota: {nota[0]}, Avaliação: {avaliacao[0]}')
